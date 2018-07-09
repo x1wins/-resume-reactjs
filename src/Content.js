685c3b8e5abbada82825998365b2bd7e714fe108
x1wins <changwoo.rhee@kooli.tv>
2018-07-09 18:00:19 +0900

    add all content class

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import About from './content/About';
 import Experiance from './content/Experiance';
+import Education from "./content/Education";
+import Skills from "./content/Skills";
+import Interests from "./content/Interests";
+import Awards from "./content/Awards";
 
 class Content extends Component {
     render() {
@@ -8,6 +12,10 @@ class Content extends Component {
             <div className="container-fluid p-0">
                 <About/>
                 <Experiance/>
+                <Education/>
+                <Skills/>
+                <Interests/>
+                <Awards/>
             </div>
         );
     }
