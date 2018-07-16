c6770364fe49471fe73fef18228c5bb0eb26c69d
x1wins <changwoo.rhee@kooli.tv>
2018-07-16 16:38:12 +0900

    add componentDidMount

--- src/index.js
+++ src/index.js
@@ -14,10 +14,34 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            error: null,
+            isLoaded: false,
             home: {
                 about: null
             }
         };
+    }
+
+    componentDidMount() {
+        fetch("https://localhost:3001/homes")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        home: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
     }
 
     render() {
