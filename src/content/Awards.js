dc3df0d01a6aa276b2a323dcff97db42ba9a64c6
RhPlus <x1wins@naver.com>
2018-07-17 10:10:40 +0900

    add Awards, Skills view

--- src/content/Awards.js
+++ src/content/Awards.js
@@ -14,41 +14,12 @@ class Awards extends Component {
                 <div className="my-auto">
                     <h2 className="mb-5">Awards &amp; Certifications</h2>
                     <ul className="fa-ul mb-0">
+                    {this.state.awards.map((award) =>
                         <li>
                             <i className="fa-li fa fa-trophy text-warning"></i>
-                            Google Analytics Certified Developer
+                            {award.title}
                         </li>
-                        <li>
-                            <i className="fa-li fa fa-trophy text-warning"></i>
-                            Mobile Web Specialist - Google Certification
-                        </li>
-                        <li>
-                            <i className="fa-li fa fa-trophy text-warning"></i>
-                            1<sup>st</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
-                        </li>
-                        <li>
-                            <i className="fa-li fa fa-trophy text-warning"></i>
-                            1<sup>st</sup>
-                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-                        </li>
-                        <li>
-                            <i className="fa-li fa fa-trophy text-warning"></i>
-                            2<sup>nd</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
-                        </li>
-                        <li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                1<sup>st</sup>
-                                Place - James Buchanan High School - Hackathon 2006
-                            </li>
-                            <li>
-                                <i className="fa-li fa fa-trophy text-warning"></i>
-                                3<sup>rd</sup>
-                                Place - James Buchanan High School - Hackathon 2005
-                            </li>
-                        </li>
+                    )}
                     </ul>
                 </div>
             </section>
--- src/content/Skills.js
+++ src/content/Skills.js
@@ -15,60 +15,14 @@ class Skills extends Component {
                     <h2 className="mb-5">Skills</h2>
 
                     <div className="subheading mb-3">Programming Languages &amp; Tools</div>
-                    <ul className="list-inline list-icons">
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-html5"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-css3"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-javascript"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-jquery"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-sass"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-less"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-bootstrap"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-wordpress"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-grunt"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-gulp"></i>
-                        </li>
-                        <li className="list-inline-item">
-                            <i className="devicons devicons-npm"></i>
-                        </li>
-                    </ul>
-
                     <div className="subheading mb-3">Workflow</div>
                     <ul className="fa-ul mb-0">
+                        {this.state.skills.map((skill) =>
                         <li>
                             <i className="fa-li fa fa-check"></i>
-                            Mobile-First, Responsive Design
+                            {skill.name}
                         </li>
-                        <li>
-                            <i className="fa-li fa fa-check"></i>
-                            Cross Browser Testing &amp; Debugging
-                        </li>
-                        <li>
-                            <i className="fa-li fa fa-check"></i>
-                            Cross Functional Teams
-                        </li>
-                        <li>
-                            <i className="fa-li fa fa-check"></i>
-                            Agile Development &amp; Scrum
-                        </li>
+                        )}
                     </ul>
                 </div>
             </section>
